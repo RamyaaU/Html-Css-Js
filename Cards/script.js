819f159f6703d8eb0b141c9537722e956961f9f2
Ramya.U <ramyayadav2016@gmail.com>
2021-03-03 11:44:19 +0530

    first commit

---
+++ Cards/script.js
@@ -0,0 +1,17 @@
+//query selectors allows to select anything 
+const panels = document.querySelectorAll('.panel')
+
+
+//foreach-arraymethod
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
