df923bfd368877a0c82f6d160489f47a740619cd
Ramya.U <ramyayadav2016@gmail.com>
2021-03-09 11:56:11 +0530

    [Ramya] Add. Added function to scroll

---
+++ Scroll Animation/script.js
@@ -0,0 +1,22 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        //method returns the object providing information about
+        //size of an element and its position
+        //relative to viewport
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
